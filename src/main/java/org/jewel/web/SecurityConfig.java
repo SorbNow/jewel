7ff54a774aea84f9b64fd0d6c7fb3dd8b50714da
Murk <Murk@1.com>
2020-04-25 21:50:02 +0300

    Add css for login page; Login-page changed to user-friendly

--- src/main/java/org/jewel/web/SecurityConfig.java
+++ src/main/java/org/jewel/web/SecurityConfig.java
@@ -6,12 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -34,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable();
         http.authorizeRequests()
 //                .antMatchers("/login-page").not().authenticated()
-                .antMatchers("/", "/user/**", "/all/**","/**").permitAll()
+                .antMatchers("/", "/user/**", "/all/**","/**", "/static/images/**").permitAll()
                 .antMatchers("/admin").hasRole("ADMIN")
                 .anyRequest().denyAll();
 
